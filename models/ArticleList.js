2c11af3682adf0ec1dcd41c06ecf28023cf9a733
jamie <b95170080@gmail.com>
2022-04-23 04:03:39 +0800

    新增model

---
+++ models/ArticleList.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const articleListSchema = new mongoose.Schema(
+    { 
+        articleContent: String,
+        articlePhoto: String,
+        userName: String,
+        userPhoto: String,
+        createAt: {
+            type: Date,
+            default:Date.now
+        }
+    },
+    {
+        versionKey: false,          // 關閉預設欄位 _v
+        collation: 'articlelist'    // 指定 Schema 名稱(不加s)
+    }
+);
+
+const ArticleList = mongoose.model('ArticleList', articleListSchema);
+module.exports = ArticleList;
