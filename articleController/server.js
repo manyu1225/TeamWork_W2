841253ae59dde9ae65400c4c1542505e6d830915
manyu1225 <manyu1225@gmail.com>
2022-05-03 20:13:39 +0800

    fix bug

--- articleController/server.js
+++ articleController/server.js
@@ -6,32 +6,31 @@ const delArticleList=require('../articleService/delArticleList');
 const ArticleList = require('../models/ArticleList');
 const errorHandle=require('../utils/errorHandle');
 const SuccessMessage = require('../utils/SuccessOutputHandle');
-const { headers } = require('../utils//libs');
+const { headers } = require('../utils/libs');
 const PORT =3005; 
 const dotenv = require('dotenv');
-dotenv.config({ path: '../config.env' });
-const DB = process.env.DATABASE.replace(
-	'<password>',
-	process.env.DATABASE_PASSWARD
-);
-mongoose
-    .connect(DB)
-    .then(() => console.log('mongodb is connected...'))
-    .catch((err) => console.log(err));
 
-const requestListener =async (req, res) =>{
-    let body ='';
-    req.on('data', (chunk) => {
-        body += chunk;
-    });
-    console.log("================================");
+dotenv.config({
+    path:'../config.env'
+});
+
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD
+  );
+   
+mongoose.connect(DB).then(()=>{
+    console.log('DB Conntection SUCCESS!');
+});
+
+const reqListener = async(req,res)=>{
+    let body ="";
+    req.on('data', (chunk)=>{
+        body+= chunk;
+    })
+
     if ( req.url === '/' && req.method === 'GET' ) {
-        res.writeHead(200,
-       {'Content-Type': 'text/html; charset=utf-8' ,
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET'}
-        );
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<h1>首頁</h1>');
         res.end();
     } else if ( req.url === '/ArticleList' && req.method === 'GET' ) {
@@ -75,10 +74,11 @@ const requestListener =async (req, res) =>{
         res.end();
     }
     return;
-}
+};
 
-const server = http.createServer(requestListener);
-server.listen(process.env.PORT || PORT, () => {
+
+const server = http.createServer(reqListener);
+server.listen(process.env.PORT, ()  => {
     if ( process.env.PORT ) {
         console.log('Deploy Heroku Successfully');
         return;
